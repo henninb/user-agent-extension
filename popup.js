f5890c09baf8b422c699db9de2fdf905420316ca
Brian Henning <henninb@msn.com>
2024-01-12 07:45:05 -0600

    wip

--- popup.js
+++ popup.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+  var originalUserAgent = navigator.userAgent;
+
+  navigator.__defineGetter__('userAgent', function(){
+      return 'PhantomJS/123';
+  });
+
   document.getElementById("uaButton").addEventListener("click", function () {
     var userAgent = navigator.userAgent;
     console.log("User Agent:", userAgent);
-    alert(userAgent);
+    alert('new:' + userAgent + '\noriginal:' + originalUserAgent);
 
-    //chrome.runtime.getBackgroundPage(function (backgroundPage) {
-    //var userAgent = backgroundPage.navigator.userAgent;
-    //console.log('User Agent:', userAgent);
-    //}
-
-    // Now you can use the userAgent string as needed
   });
+
 });
